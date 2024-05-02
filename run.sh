0c4b4043e5effc88c3b38c53d1660ae626b6ae57
satyachillale <satya.narayana2406@gmail.com>
2024-05-02 11:55:33 -0400

    changes

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 python3 paradigm_dl_proj_satya_2.py
 cd mcvd
 CUDA_VISIBLE_DEVICES=0 python3 main.py --config configs/paradigm-moving-objects.yml --data_path ../../dataset --exp paradigm-moving-objects-out --ni
-python3 test_diffusion_hidden.py ../../dataset paradigm-moving-objects-out/logs/checkpoint_100.pt ../results
+python3 test_diffusion_hidden.py ../../dataset paradigm-moving-objects-out/logs/checkpoint_150000.pt ../results
 cd ..
 python3 run_segmentation.py paradigm_segmentation.pth results/predictions-15000-17000-val/
